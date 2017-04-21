744c93473a66e221f834ffce76fdc373d3e70333
vaulverin <vauulin@gmail.com>
2017-04-21 13:14:50 +0000

    feedback list - done

--- basic/views/feedback/list.php
+++ basic/views/feedback/list.php
@@ -12,9 +12,10 @@ $this->title = 'My Yii Application';
     <div class="body-content">
         <h1>Feedback list is HERE!</h1>
         <div class="row margin-null">
-            <?php ListView::widget([
+            <?= ListView::widget([
                     'dataProvider' => $dataProvider,
-                    'itemView' => 'listItem'
+                    'itemView' => 'listItem',
+                    'emptyText' => 'Список пуст',
             ]);?>
         </div>
     </div>
